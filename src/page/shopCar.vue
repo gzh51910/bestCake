df9641ce89c9b9f7d4798c62180d2f64af919cbc
H5-WSW <1360608462@qq.com>
2019-11-16 18:02:49 +0800

    wsw购物车完善

--- src/page/shopCar.vue
+++ src/page/shopCar.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="shopCart">
+    {{selectnum}}
     <!-- 1shoppingCart -->
     <div class="shopCart-list clear">
-      <ul>
+      <ul ref="wrap">
         <li v-for="item in shoplist" :key="item.ID">
           <!-- (1)购物车内容 -->
-      
+
           <div class="shopCart-content clear">
-            <!-- 选中按钮 -->
+            <!-- 单选框 -->
             <div class="selected">
-              <img src="../assets/imgs/cart-mw_firm_duihao_1.jpg" alt />
+              <img :src="selectimg" alt @click="cartSelected($event)" data-select="true" />
             </div>
             <!-- 商品信息 -->
             <div class="list-product">
               <!-- 商品图 -->
-              <img class="list-product-l" :src="getListUrl(item.Name,item.Brand)" alt />
+              <img class="list-product-l" :src="getListUrl(item.Name,item)" alt />
 
               <!-- 商品（名称、规格、数量、单价） -->
               <div class="list-product-r">
@@ -30,9 +31,11 @@
                       <span class="num-add" @click="addNum(item)">+</span>
                     </div>
                   </div>
-                    <!-- 价格 -->
+                  <!-- 价格 -->
                   <div class="shopCart-des-price">{{item.CurrentPrice*item.num}}.00</div>
-                  <div class="shopCart-des-price-dis"> <s> {{item.CurrentPrice*item.num/0.8}}.00</s></div>
+                  <div class="shopCart-des-price-dis">
+                    <s>{{item.CurrentPrice*item.num/0.8}}.00</s>
+                  </div>
                 </div>
               </div>
             </div>
@@ -54,7 +57,18 @@
       </div>
       <div class="hotList">
         <ul class="clear">
-          <li>
+          <li v-for="(item,idx) in list" :key="idx">
+            <img class="list-img-top" :src="item.img" alt />
+            <h4>{{item.Name}}</h4>
+            <div class="des">
+              <span class="price">
+                {{item.CurrentPrice}}
+                <i>{{item.Size}}</i>
+              </span>
+              <img class="shopIcon" src="../assets/imgs/cart_firm_gwc.jpg" alt @click="addCart(item)"/>
+            </div>
+          </li>
+          <!-- <li>
             <img
               class="list-img-top"
               src="https://res.bestcake.com\m-images\cart\mw_firm_sq.jpg"
@@ -66,7 +80,7 @@
                 168.00
                 <i>/1盒</i>
               </span>
-              <img class="shopIcon" src="../assets/imgs/cart_firm_gwc.jpg" alt />
+              <img class="shopIcon" src="../assets/imgs/cart_firm_gwc.jpg" alt @click="toshopcar"/>
             </div>
           </li>
 
@@ -82,7 +96,7 @@
                 68.00
                 <i>/16粒装</i>
               </span>
-              <img class="shopIcon" src="../assets/imgs/cart_firm_gwc.jpg" alt />
+              <img class="shopIcon" src="../assets/imgs/cart_firm_gwc.jpg" alt @click="addCart" />
             </div>
           </li>
 
@@ -100,7 +114,7 @@
               </span>
               <img class="shopIcon" src="../assets/imgs/cart_firm_gwc.jpg" alt />
             </div>
-          </li>
+          </li>-->
         </ul>
       </div>
     </div>
@@ -108,8 +122,14 @@
     <div class="totalPrices">
       <div class="totalPrices-l">
         <span>
-          <img src="../assets/imgs/cart-mw_firm_duihao_1.jpg" alt />全选
-          <i class="totalPrices-clear">清空</i>
+          <!-- 全选框 -->
+          <img
+            ref="selectall"
+            src="../assets/imgs/cart-mw_firm_duihao_1.jpg"
+            alt
+            @click="cartAllSelected($event)"
+          />全选
+          <i class="totalPrices-clear" @click="clesrSelect">清空</i>
         </span>
       </div>
 
@@ -132,29 +152,62 @@
       </div>
     </div>
     <!-- {{aaa(index)}} -->
+    <!-- 添加购物车提示 -->
+    <div class="pop" v-show="showpop">已添加到购物车</div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      data: []
+      datamsg: [],
+      arr: [],
+      list: [
+        {
+          img: "https://res.bestcake.com\\m-images\\cart\\mw_firm_sq.jpg",
+          Name: "伴手礼系列-吉致生巧",
+          CurrentPrice: 168,
+          Size: "一盒",
+          num: 1
+        },
+        {
+          img: "https://res.bestcake.com\\m-images\\cart\\mw_firm_nzt_v1.jpg",
+          Name: "伴手礼系列-吉致牛轧糖",
+          CurrentPrice: 68,
+          Size: "/16粒装",
+          num: 1
+        },
+        {
+          img: "https://res.bestcake.com\\m-images\\cart\\mw_firm_pf_v1.jpg",
+          Name: "伴手礼系列-吉致泡芙",
+          CurrentPrice: 88,
+          Size: "0.8磅",
+          num: 1
+        }
+      ],
+      ifselectall: true,
+      selectimg: require("../assets/imgs/cart-mw_firm_duihao_1.jpg"), //选中时的图（初始为默认选中）
+      selectnum: 0,
+      showpop: false,
     };
   },
   methods: {
-    getListUrl(name, brand) {
-      if (brand == "卡思客") {
+    getListUrl(name, item) {
+      console.log(item);
+
+      if (item.Brand == "卡思客") {
         return `https://res.bestcake.com/m-images/ww/jd/${name}.png?`;
-      } else if (brand == "女神系列") {
+      } else if (item.Brand == "女神系列") {
         return `https://res.bestcake.com/m-images/ww/ns/${name}.jpg?`;
-      } else if (brand == "极致蛋糕") {
+      } else if (item.Brand == "极致蛋糕") {
         return `https://res.bestcake.com/m-images/ww/jz/${name}.png?`;
-      } else {
+      } else if (item.Brand == "乳品系列") {
         return `https://res.bestcake.com/m-images/ww/rp/${name}.jpg?`;
+      } else {
+        return item.img;
       }
     },
     reduceNum(item) {
-    
       if (item.num > 0) {
         if (item.num == 1) {
           item.num = 1;
@@ -163,34 +216,110 @@ export default {
           // item.CurrentPrice=
         }
       }
-      console.log("num-1=",item.num);
+      console.log("num-1=", item.num);
     },
-    addNum(item){
-      if(item.num>0){
-        item.num ++;
+    addNum(item) {
+      if (item.num > 0) {
+        item.num++;
       }
-      console.log("num+1=",item.num);
-      
+      console.log("num+1=", item.num);
+    },
+    addCart(item) {
+      this.showpop=true;
+      setTimeout(()=>{
+        this.showpop=false
+      },1500)
+      if (localStorage.getItem("ShoppingCart")) {
+        this.arr = JSON.parse(localStorage.getItem("ShoppingCart"));
+      } else {
+        this.arr = [];
+      }
+      let newdata = JSON.parse(JSON.stringify(item));
+
+      let str = JSON.stringify(this.arr);
+      if (str.indexOf(newdata.Name) == -1) {
+        this.arr.unshift(newdata);
+      } else {
+        this.arr.forEach(ele => {
+          if (ele.Name == newdata.Name) {
+            ele.num += 1;
+          }
+        });
+      }
+
+      localStorage.setItem("ShoppingCart", JSON.stringify(this.arr));
+      let updata = JSON.parse(localStorage.getItem("ShoppingCart"));
+      this.datamsg = updata;
+    },
+    // （1）全选
+    cartAllSelected(event) {
+      if (this.ifselectall) {
+        //ifselectall=true
+        // 实现点击时替换不全选的图--模块化引入本地图片路径
+        event.target.src = require("../assets/imgs/cart-mw_firm_duihao_2.jpg"); //  全选被点击后不勾选，event.target=当前被点击的img
+        this.selectimg = require("../assets/imgs/cart-mw_firm_duihao_2.jpg"); //全选不勾选，所有商品也不勾选
+        this.ifselectall = !this.ifselectall; //不被勾选，为false
+        console.log(this.$ref);
+      } else {
+        event.target.src = require("../assets/imgs/cart-mw_firm_duihao_1.jpg");
+        this.selectimg = require("../assets/imgs/cart-mw_firm_duihao_1.jpg");
+        this.ifselectall = !this.ifselectall;
+      }
+      console.log("event", event.target.src);
+      console.dir(this.$refs.wrap);
+    },
+    // （2）单选
+    cartSelected(event) {
+      if (event.target.dataset.select == "true") {
+        //自定义属性data-select==select,显示当前单选框是否为勾选，true为勾选
+        // 点击被勾选的单选框，即取消勾选，计算当前被勾选中的单选框数量
+        this.selectnum--;
+        // 判断当前被勾选的单选框的数量=？当前商品数相同，不等则全选框取消勾选
+        if (this.selectnum != this.datamsg.length) {
+          this.$refs.selectall.src = require("../assets/imgs/cart-mw_firm_duihao_2.jpg"); //全选框
+        }
+        event.target.src = require("../assets/imgs/cart-mw_firm_duihao_2.jpg"); //当前被点击的单选框，自己本身也要取消选中
+        event.target.dataset.select = "false"; //此时该单选框为不被选中状态
+      } else {
+        // 当前单选框为没勾选状态
+        this.selectnum++;
+
+        if (this.selectnum == this.datamsg.length) {
+          this.$refs.selectall.src = require("../assets/imgs/cart-mw_firm_duihao_1.jpg");
+        }
+        event.target.src = require("../assets/imgs/cart-mw_firm_duihao_1.jpg");
+        event.target.dataset.select = "true";
+      }
+    },
+    // （3）清空
+    clesrSelect() {
+      if (this.ifselectall) {
+        confirm("是否清空")
+        localStorage.setItem("ShoppingCart", "");
+        this.datamsg=[];
+      }
     }
   },
   computed: {
     shoplist() {
-      return this.data;
+      return this.datamsg;
     },
-    totalPrices(){
-      let total=0;
-      this.shoplist.forEach((item)=>{
-        total+=item.CurrentPrice*item.num;
-      })
+    totalPrices() {
+      let total = 0;
+      console.log(this, "====");
+
+      this.shoplist.forEach(item => {
+        total += item.CurrentPrice * item.num;
+      });
       return total.toFixed(2);
     },
-    totalPricesDis(){
-      let discount=0;
-      this.shoplist.forEach((item)=>{
-        discount+=(item.CurrentPrice*item.num*(1-0.8));
-      })
+    totalPricesDis() {
+      let discount = 0;
+      this.shoplist.forEach(item => {
+        discount += item.CurrentPrice * item.num * (1 - 0.8);
+      });
       return discount.toFixed(2);
-    },
+    }
     // aaa(index){
     //   return function totalPrices(){
     //     let total=index;
@@ -202,7 +331,12 @@ export default {
     // }
   },
   created() {
-    this.data = JSON.parse(localStorage.getItem("ShoppingCart"));
+    if (localStorage.getItem("ShoppingCart")) {
+      this.datamsg = JSON.parse(localStorage.getItem("ShoppingCart"));
+      this.selectnum = this.datamsg.length; //获取目前在购物车的商品数据的数量的长度
+    } else {
+      this.datamsg = [];
+    }
   }
 };
 </script>
@@ -330,7 +464,7 @@ export default {
   line-height: 4vw;
   margin-top: 2.66vw;
 }
-.shopCart-des-price-dis{
+.shopCart-des-price-dis {
   float: left;
   font-size: 3vw;
   color: #ccc;
@@ -533,5 +667,20 @@ export default {
   height: 0;
   display: block;
   visibility: hidden;
+}
+// 提示
+.pop {
+  width: 32%;
+  margin: auto;
+  height: 5.5vw;
+  line-height: 5vw;
+  padding: 4vw;
+  color: #666;
+  background: rgb(231, 239, 252);
+  position: fixed;
+  top: 40%;
+  left: 0;
+  right: 0;
+  border-radius: 5%;
 }
 </style>
